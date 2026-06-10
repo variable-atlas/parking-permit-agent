0fde40869943c23e8d34da1d9f91851339540351
Victoria Turner <rothwell.victoria90@gmail.com>
2026-06-10 12:10:04 +0100

    Remove custom chat widget, keep MIAW and Stripe integration

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import BinCollectionCard from './components/BinCollectionCard'
 import ParkingPermitCard from './components/ParkingPermitCard'
 import RoadworksCard from './components/RoadworksCard'
 import SalesforceChat from './components/SalesforceChat'
-import ChatWidget from './components/ChatWidget'
 import { resident } from './data/residentData'
 import { initiateLogin, handleCallback, getStoredAuth, logout } from './services/salesforceAuth'
 import { fetchParkingPermit } from './services/salesforceService'
@@ -154,8 +153,6 @@ export default function App() {
             </span>
           </div>
 
-
-          <ChatWidget />
 
           <div className={styles.chatInvite}>
             <div className={styles.chatInviteLeft}>
